941c3a51c89480b75335951202cd415baad291d1
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2022-11-28 12:20:56 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,5 +1,8 @@
 const solution = (price) => {
-    const pay = price>=500000 ? Math.floor(price*0.8) : price>=300000 ? Math.floor(price*0.9) : price>=100000 ? Math.floor(price*0.95) : price
+    const pay = 
+        price>=500000 ? Math.floor(price*0.8) : 
+        price>=300000 ? Math.floor(price*0.9) : 
+        price>=100000 ? Math.floor(price*0.95) : price
 
     return pay
 }
