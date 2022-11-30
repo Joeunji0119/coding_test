be8021f2a1aafa5675a298aeebb4614d917295fe
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2022-11-30 11:26:40 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,22 +1,3 @@
 const solution = (hp) => {
-    
-    if(hp>=5 && hp%5===0) {
-        return hp/5
-    }
-    if(hp>=5 && hp%5 !==0){
-        if(hp%5 < 3){
-            return Math.floor(hp/5) + hp%5
-        }
-        if(hp%5 >=3){
-            return Math.ceil(hp/5) + hp%5-3
-        }
-    }
-    if(hp<5){
-        if(hp>=3){
-            return Math.floor(hp/3) + hp%3
-        }
-        if(hp<3){
-            return hp
-        }
-    }
+    return Math.floor(hp/5) + Math.floor((hp%5)/3) + (hp%5)%3
 }
