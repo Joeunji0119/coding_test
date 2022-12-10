2aaf2148186c1f0c0e516a9b6277573cd93d76d1
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2022-12-10 10:59:17 +0900

    [level 0] Title: 모스부호 (1), Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -1,18 +1,4 @@
-const solution = (letter) => {
-    
-    const letterArr = letter.split(" ")
-    const answer = []
-    
-    morse = { 
-    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
-    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
-    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
-    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
-    '-.--':'y','--..':'z'
-    }
-    
-    for(i=0; i<letterArr.length; i++){
-        answer.push(morse[letterArr[i]])
-    }
-    return answer.join("")
+function solution(letter) {
+    var answer = '';
+    return answer;
 }
