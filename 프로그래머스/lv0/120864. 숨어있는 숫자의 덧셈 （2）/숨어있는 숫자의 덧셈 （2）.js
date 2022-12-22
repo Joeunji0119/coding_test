fdb364b76f1bacaa12221326eae95582b04e84de
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2022-12-22 22:55:34 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 2.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,15 @@
+function solution(my_string) {
+    let result = 0
+    
+    for(let i=0; i<my_string.length; i++) {
+        let thisNum = ''
+        
+        while(!Number.isNaN(Number(my_string[i]))) {
+            thisNum+= my_string[i]
+            console.log(thisNum)
+            i++
+        }
+        result+= +thisNum
+    }
+    return result
+}
