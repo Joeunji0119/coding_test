51971f233e3743e564ac8b7f95d4cd9b5e1529d9
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2022-12-28 23:04:04 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,10 +1,40 @@
-const solution = ch => {
-  const func = cp => {
-    if(cp < 10){
+function solution(chicken) {
+    let Arr = [] // 몫
+    let Arr2 = [] // 나머지
+
+    const self = (chicken) => {
+        
+        if (chicken < 10) {
+             const ArrSum = Arr.reduce((a,b)=>a+b)
+             console.log(Number(ArrSum)
+             const Arr2Sum = Arr2.reduce((a,b)=>a+b)
+            if(Arr2 >= 9){
+                if(Arr2%10 === 9){
+                    return ArrSum.join("") +  Math.floor(Arr2/10) + 1
+                }
+                if(Arr2%10 < 9){
+                    return Arr2Sum.join("") + Math.floor(Arr2/10)
+                }
+            }
+            return Arr.join("")
+        }
+        
+        if(chicken >= 10){
+            let a = Math.floor(chicken/10)
+            let b = chicken%10
+            Arr.push(a)
+            Arr2.push(b)
+            return self(a)
+          }
+    }
+    
+    if (chicken < 10){
         return 0
     }
-    let sv = Math.floor(cp/10)
-    return sv + func(sv + (cp % 10))
-  }
-  return func(ch)
+    if(chicken === 10){
+        return 1
+    }
+    if(chicken>10){
+        return self(chicken)
+    }
 }
