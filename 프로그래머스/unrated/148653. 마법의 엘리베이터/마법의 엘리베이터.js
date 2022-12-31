033ff1c0537db909c033c32fb21feeb95d627a3a
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2022-12-31 15:16:59 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,33 +1,6 @@
 function solution(storey) {
-    
-    const map = (x) => Number(x);
-    let Arr = Array.from(String(storey),map)
-    let count = 0
-    
-    for (let i=Arr.length-1; i>=0; i--){
-      if( i === 0){
-        console.log(Arr[0])
-        return Arr[0]>5 ? count += 10-Arr[0]+1 : count += Arr[0]
-      }
-      
-        if(0<Arr[i] && Arr[i]<5){
-          count += Arr[i]
-        }
-        
-        if(Arr[i]>5){
-          count += 10-Arr[i]
-          console.log(count)
-          Arr.splice(i-1,1,Arr[i-1]+1)
-        }
-      
-      if(Arr[i]===5){
-        if(Arr[i-1]>=5){
-          count += 5
-          Arr.splice(i-1,1,Arr[i-1]+1)
-        }
-        if(Arr[i-1]<5){
-          count += 5
-        }
-      }
-    }
+    if (storey < 5) return storey;
+    const r = storey % 10;
+    const m = (storey - r) / 10;
+    return Math.min(r + solution(m), 10 - r + solution(m + 1));
 }
