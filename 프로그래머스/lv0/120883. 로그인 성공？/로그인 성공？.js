ce70b52cd5f947ed0eff5a7dced5302bda42d9a9
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2023-01-09 13:45:16 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,7 +1,6 @@
 function solution(id_pw, db) {
     let map = new Map(db);
-    const id = id_pw[0]
-    const pw = id_pw[1]
+    const [id,pw] = id_pw
     
     if(map.get(id) === pw) return "login"
     if(map.has(id) && map.get(id) !== pw) return "wrong pw"
