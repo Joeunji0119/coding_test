358bdef25194177a32a87e7583a2ee7765b0cc02
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2025-11-30 20:42:29 +0900

    [level 0] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,11 @@
+function solution(n, slicer, num_list) {
+    let [a,b,c] = slicer;
+    
+    if(n ===1) return num_list.slice(0,b +1)
+    if(n ===2) return num_list.slice(a,num_list.length)
+    if(n ===3) return num_list.slice(a,b +1)
+    if(n ===4) {
+        let area = num_list.slice(a,b+1)
+        return area.filter((e, index)=> index % c === 0)    
+    }
+ }
