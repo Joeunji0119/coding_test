479fc08e94c0b01dd2f55ffda8ba9aed5e78fa55
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2025-12-02 00:21:41 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.16 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+
+    let a = 0;
+    let b = 0;
+    
+    for(let i=0;i<num_list.length;i++){
+        if(i%2 ===0) {
+            a = a + num_list[i]
+        } else {
+            b = b + num_list[i]
+        }
+    }
+    
+    if(a === b) return a
+    if(a>b)return a
+    return b
+}
