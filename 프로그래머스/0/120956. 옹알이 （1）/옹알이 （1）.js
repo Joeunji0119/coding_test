6bf074540ff672166cbf5ffe45269d867978ba27
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2025-12-01 20:57:52 +0900

    [level 0] Title: 옹알이 (1), Time: 0.70 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,27 @@
+
+ let restFunc = (wordArr,i)=>wordArr.slice(i+1,word.length).join("")
+
+const canSpell = (word) => {
+    let spall = ["aya","ye","woo","ma"]
+    const wordArr = [...word]
+    let a = [];
+    for(let i=0; i<wordArr.length; i++){
+        a.push(wordArr[i])
+        let johap = a.join("")
+        if(spall.includes(johap)){
+            a = [];
+            spall = spall.filter((e)=> e !== johap)
+
+            if(wordArr.length-1 === i) return true
+        }
+    }
+    
+    return false
+}
+
+function solution(babbling) {
+    return babbling.reduce((a,c)=> {
+        if(canSpell(c)){ a = a + 1 }
+       return a
+    },0)
+}
