96f87bbb3efc6fbd39ad075e8cca8889d7d3296d
joeunji <95282989+Joeunji0119@users.noreply.github.com>
2025-12-13 21:05:00 +0900

    [level 1] Title: 햄버거 만들기, Time: 197.58 ms, Memory: 97.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,17 @@
+function solution(ingredient) {
+    let stack = [];
+    let count = 0;
+    
+    for(let i=0; i<ingredient.length; i++){
+        stack.push(ingredient[i])
+
+        if(stack.length>=4){
+            const word = stack.slice(-4).join("")
+            if(word === '1231'){
+                count++;
+                stack.splice(-4)
+            }
+        }
+    }
+    return count
+}
